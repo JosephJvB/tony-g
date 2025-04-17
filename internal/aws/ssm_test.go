bc052165bd41ca572e0eaa0640bee39c395cac22
Joe van Bohemen <joevanbo@protonmail.com>
2025-04-17 11:55:16 +0100

    test(ssm): can load params from local

---
+++ internal/aws/ssm_test.go
@@ -0,0 +1,38 @@
+package aws
+
+import (
+	"os"
+	"testing"
+)
+
+func init() {
+	os.Setenv("AWS_REGION", "eu-west-2")
+}
+
+func TestSsm(t *testing.T) {
+	t.Run("can load all params", func(t *testing.T) {
+		t.Skip("skip test relying on AWS --profile joe calling live API")
+		sc := NewClient()
+
+		sc.LoadParameterValues()
+
+		list := []Parameter{
+			sc.GoogleClientEmail,
+			sc.GooglePrivateKey,
+			sc.SpotifyClientId,
+			sc.SpotifyClientSecret,
+			sc.SpotifyRefreshToken,
+		}
+
+		missing := []string{}
+		for _, p := range list {
+			if p.Value == "" {
+				missing = append(missing, p.Name)
+			}
+		}
+
+		if len(missing) > 0 {
+			t.Errorf("Failed to load params:%v\n", missing)
+		}
+	})
+}
