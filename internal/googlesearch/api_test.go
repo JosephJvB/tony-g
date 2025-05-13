80b9b5a5d88d0a93ad3ebb391dbf5de2fee3099c
Joe van Bohemen <joevanbo@protonmail.com>
2025-05-13 14:37:24 +0100

    test out two cx values

--- internal/googlesearch/api_test.go
+++ internal/googlesearch/api_test.go
@@ -1,10 +1,12 @@
 package googlesearch
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 
 	"github.com/joho/godotenv"
+	"google.golang.org/api/customsearch/v1"
 )
 
 func TestGoogleSearchApi(t *testing.T) {
@@ -32,6 +34,53 @@ func TestGoogleSearchApi(t *testing.T) {
 
 		if result != "https://open.spotify.com/track/0jv5VgdENAPV7lHtBlsaXE" {
 			t.Errorf("Expected \"https://open.spotify.com/track/0jv5VgdENAPV7lHtBlsaXE\" received \"%s\"", result)
+		}
+	})
+
+	t.Run("try with cse service", func(t *testing.T) {
+		// t.Skip("Skip calling real CustomSearch API")
+
+		err := godotenv.Load("../../.env")
+		if err != nil {
+			t.Errorf("Error loading .env file")
+		}
+
+		cfg := Config{
+			ApiKey: os.Getenv("GOOGLE_SEARCH_API_KEY"),
+			Cx:     os.Getenv("GOOGLE_SEARCH_CX"),
+		}
+
+		client := NewClient(cfg)
+
+		cse := customsearch.NewCseService(client.svc)
+
+		q := "Blood on the Fang clipping."
+		resp, err := cse.List().
+			Cx(os.Getenv("GOOGLE_SEARCH_CX_SPECIFIC")).
+			Q(q).
+			Num(1).
+			Do()
+
+		if err != nil {
+			t.Fatalf("cse custom search failed %s", err.Error())
+		}
+
+		b, err := json.Marshal(resp)
+		if err != nil {
+			t.Fatalf("failed to marshal response %s", err.Error())
+		}
+
+		err = os.WriteFile("../../data/google-customsearch.json", b, 0666)
+		if err != nil {
+			t.Fatalf("failed to write file %s", err.Error())
+		}
+
+		if len(resp.Items) == 0 {
+			t.Fatalf("failed to find any search results")
+		}
+
+		if resp.Items[0].Link != "https://open.spotify.com/track/0jv5VgdENAPV7lHtBlsaXE" {
+			t.Fatalf("failed to find correct track url")
 		}
 	})
 }
